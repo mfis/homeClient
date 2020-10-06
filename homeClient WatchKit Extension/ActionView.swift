2deee97912bc194c03012f42340b8f1809b5111c
mfis <fischermatthias1980@googlemail.com>
2020-10-06 21:50:25 +0200

    pin code view

--- homeClient WatchKit Extension/ActionView.swift
+++ homeClient WatchKit Extension/ActionView.swift
@@ -12,9 +12,17 @@ struct ActionView: View {
     @State var place : HomeViewPlaceModel
     @EnvironmentObject private var userData : UserData
     @Environment(\.presentationMode) var presentation
+    @State private var showModal = false
+    @State var pin = ""
+    let pinLength = 6
     
     var body: some View {
         Form {
+            Button("Show Modal") {
+                self.showModal.toggle()
+            }.sheet(isPresented: $showModal, onDismiss: {print("MODAL DISMISSED: " + pin)}) {
+                PinView(length : pinLength, pin: $pin, showModal: self.$showModal)
+            }
             List(place.actions) { action in
                 HStack {
                     Spacer()
@@ -30,8 +38,16 @@ struct ActionView: View {
                     }
                 }
             }
-        }.navigationBarTitle("Aktion")
+        }.navigationBarTitle("Aktion").onDisappear(perform: {
+            pin = ""
+        })
     }
     
 }
 
+
+struct ActionView_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ homeClient WatchKit Extension/PinView.swift
@@ -0,0 +1,82 @@
+//
+//  PinView.swift
+//  homeClient WatchKit Extension
+//
+//  Created by Matthias Fischer on 05.10.20.
+//
+
+import SwiftUI
+
+struct PinView: View {
+    
+    var length:  Int
+    @Binding var pin:  String
+    @Binding var showModal: Bool
+    
+    var body: some View {
+        GeometryReader { proxy in
+            VStack{
+                HStack(spacing: 4) {
+                    ForEach(0 ..< length) { index in
+                        Circle()
+                            .fill(Color.primary.opacity(index < pin.count
+                                                            ? 0.9
+                                                            : 0.3))
+                            .frame(width: 10,
+                                   height: 10,
+                                   alignment: .center)
+                    }
+                }.padding(.bottom, 5)
+                HStack {
+                    Button("1"){press("1")}.buttonStyle(PinButton())
+                    Button("2"){press("2")}.buttonStyle(PinButton())
+                    Button("3"){press("3")}.buttonStyle(PinButton())
+                }
+                HStack {
+                    Button("4"){press("4")}.buttonStyle(PinButton())
+                    Button("5"){press("5")}.buttonStyle(PinButton())
+                    Button("6"){press("6")}.buttonStyle(PinButton())
+                }
+                HStack {
+                    Button("7"){press("7")}.buttonStyle(PinButton())
+                    Button("8"){press("8")}.buttonStyle(PinButton())
+                    Button("9"){press("9")}.buttonStyle(PinButton())
+                }
+                HStack {
+                    Button("X"){pin = ""; showModal = false}.buttonStyle(PinButton())
+                    Button("0"){press("9")}.buttonStyle(PinButton())
+                    Button("←"){pin = ""}.buttonStyle(PinButton())
+                }
+            }
+        }.edgesIgnoringSafeArea(.bottom)
+    }
+    
+    func press(_ number : String){
+        pin = pin + number
+        if(pin.count==length){
+            showModal = false
+        }
+    }
+}
+
+struct PinButton: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 10, maxHeight: 15)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.init(hexString: "17202a"))
+            .cornerRadius(4)
+            .scaleEffect(configuration.isPressed ? 0.5 : 1.0)
+            .font(.headline)
+    }
+}
+
+struct PinView_Previews: PreviewProvider {
+    static var previews: some View {
+        PinView(length: 6, pin: .constant(""), showModal: .constant(true)).toolbar(content: {
+            
+        })
+    }
+}
